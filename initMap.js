ce325b1d9613d5f825047cc991ca845409aa1ba9
gino.bai <yo20703@gmail.com>
2022-01-19 15:47:24 +0800

    add 5 marker

--- initMap.js
+++ initMap.js
@@ -1,15 +1,59 @@
-
+var map;
+var markers = [];
 function initMap() {
-  
-  const uluru = { lat: -25.344, lng: 131.036 };
-  
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: uluru,
-  });
-  
-  const marker = new google.maps.Marker({
-    position: uluru,
-    map: map,
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 17,
+    center: {
+      lat: 25.0336962,
+      lng: 121.5643673
+    }
   });
 }
+
+function go() {
+  for (var i = 0; i < 5; i++) {
+    addMarker(i);
+  }
+}
+
+function addMarker(e) {
+  setTimeout(function() {
+    let infowindow = new google.maps.InfoWindow({
+      content: 'test123' 
+    });
+
+    markers.push(new google.maps.Marker({
+      position: {
+        lat: position[e].lat,
+        lng: position[e].lng
+      },
+
+      map: this.map,
+      icon: {
+        path: google.maps.SymbolPath.CIRCLE,
+        scale: 10
+      },
+      draggable: false,
+      //label: position[e].label,
+    }).addListener("click", e => {
+      infowindow.open({
+        anchor: this,
+        map,
+        shouldFocus: false,
+      });
+    }));
+  }, e * 150);
+}
+
+go();
+
+
+var position = [
+  {label:'A', lat:25.0336962, lng:121.5643673},
+  {label:'B', lat:25.0333698, lng:121.5641564},
+  {label:'C', lat:25.033899, lng:121.564329},
+  {label:'D', lat:25.0338407, lng:121.5645269},
+  {label:'E', lat:25.0336377, lng:121.5645727}
+];
+
+
